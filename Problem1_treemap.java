26d925059336a4c03aea212b971d4f01ea1682c8
Aiko Lin <ailin@adobe.com>
2017-12-29 14:56:28 -0500

    Solve problem in the treemap solution by using arraylist

--- Problem1_treemap.java
+++ Problem1_treemap.java
@@ -5,9 +5,15 @@
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        TreeMap<Integer, Integer> tm = new TreeMap<Integer, Integer>();
+        TreeMap<Integer, ArrayList<Integer>> tm = new TreeMap<Integer, ArrayList<Integer>>();
         for (int i = 0; i < nums.length; i++) {
-            tm.put(nums[i], i);
+            if (tm.containsKey(nums[i])) {
+                tm.get(nums[i]).add(i);
+            } else {
+                ArrayList<Integer> list = new ArrayList<Integer>();
+                list.add(i);
+                tm.put(nums[i], list);
+            }
         }
         Map.Entry left = tm.firstEntry();
         Map.Entry right = tm.lastEntry();
@@ -18,17 +24,30 @@ class Solution {
                 right = tm.lowerEntry((Integer)right.getKey());
             }
             if ((int)right.getKey() == remainder) {
-                return new int[]{Math.min((int)left.getValue(), (int)right.getValue()), Math.max((int)left.getValue(), (int)right.getValue())};
+                return getAnswerSet(left, right);
             }
-            remainder = target - (int)right.getKey();
 
+            remainder = target - (int)right.getKey();
             while (remainder > (int)left.getKey()) {
                 left = tm.higherEntry((Integer)left.getKey());
             }
+
             if ((int)left.getKey() == remainder) {
-                return new int[]{Math.min((int)left.getValue(), (int)right.getValue()), Math.max((int)left.getValue(), (int)right.getValue())};
+                return getAnswerSet(left, right);
             }
             remainder = target - (int)left.getKey();
         }
+    }
+
+    static int[] getAnswerSet(Map.Entry left, Map.Entry right) {
+        ArrayList<Integer> a = (ArrayList<Integer>)left.getValue();
+        ArrayList<Integer> b = (ArrayList<Integer>)right.getValue();
+        return (int)left.getKey() == (int)right.getKey() ?
+            getOrderedSet(a.get(0), a.get(1)):
+            getOrderedSet(a.get(0), b.get(0));
+    }
+
+    static int[] getOrderedSet(Integer a, Integer b) {
+        return new int[]{Math.min((int)a, (int)b), Math.max((int)a, (int)b)};
     }
 }
